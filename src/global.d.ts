3ca93a67a534788744f8d6199c866429b28f670d
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2022-03-04 01:15:33 -0500

    Update packages, config and types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="@sveltejs/kit" />
-
-type Note = {
-  id: string;
-  title: string;
-  content?: string;
-  hidden?: boolean;
-  createdAt?: Date;
-  updatedAt?: Date;
-};
-
-type Icon = `add` | `generate` | `save` | `load` | `more` | `hamburger`;
