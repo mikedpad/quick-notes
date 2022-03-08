7f7dc1b9ba46b3451b3da5de81b64e04dc4dcf1b
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2022-03-07 23:58:48 -0500

    Update store.ts

--- src/data/store.ts
+++ src/data/store.ts
@@ -1,18 +1,15 @@
-// Types
 import type { Note } from 'src/types/note';
 import type { Writable } from 'svelte/store';
-
-// Imports
 import { writable } from 'svelte/store';
 
 // Note
 export const notes: Writable<Note[]> = writable([]);
-export function addNote(note: Note) {
-  notes.update(notes => [...notes, note]);
-}
-export function removeNote(id: string) {
-  notes.update(notes => notes.filter(n => n.id !== id));
-}
+// export function addNote(note: Note) {
+//   notes.update(notes => [...notes, note]);
+// }
+// export function removeNote(id: string) {
+//   notes.update(notes => notes.filter(n => n.id !== id));
+// }
 
 // State
 export const modalOpen: Writable<boolean> = writable(false);
